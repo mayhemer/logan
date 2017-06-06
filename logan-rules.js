7e245f667f29c326bc19e847b7eea23e3abaeaf3
Honza Bambas :mayhemer <honzab@firemni.cz>
2017-06-06 21:03:02 +0200

    Backup

--- logan-rules.js
+++ logan-rules.js
@@ -1,3 +1,7 @@
+/******************************************************************************
+ * HttpChannelParent
+ ******************************************************************************/
+
 logan.rule("Creating HttpChannelParent [this=%p]", function(ptr)
 {
   this.thread.httpchannelparent = this.create(ptr, "HttpChannelParent");
@@ -7,20 +11,140 @@ logan.rule("Destroying HttpChannelParent [this=%p]", function(ptr)
   this.destroy(ptr);
 });
 
+/******************************************************************************
+ * nsHttpChannel
+ ******************************************************************************/
 
-logan.rule("Creating nsHttpChannel [this=%p]", function(ptr) 
+logan.rule("Creating nsHttpChannel [this=%p]", function(ptr)
 {
   this.thread.httpchannel = this.create(ptr, "nsHttpChannel");
   if (this.thread.httpchannelparent) {
     this.link(this.thread.httpchannelparent, this.thread.httpchannel);
   }
 });
-logan.ruleIf("uri=%s", function() { return !!this.thread.httpchannel; }, function(url) 
+logan.ruleIf("uri=%s", state => state.thread.httpchannel, function(url)
 {
   this.prop(this.thread.httpchannel, "url", url);
 });
-logan.rule("Destroying nsHttpChannel [this=%p]", function(ptr) 
+logan.rule("Destroying nsHttpChannel [this=%p]", function(ptr)
 {
   this.destroy(ptr);
 });
+logan.summaryProps("nsHttpChannel", ["state", "url"]);
 
+/******************************************************************************
+ * nsHttpTransaction
+ ******************************************************************************/
+
+logan.rule("Creating nsHttpTransaction @%p", function(ptr)
+{
+  this.thread.httptransaction = this.create(ptr, "nsHttpTransaction");
+});
+logan.rule("nsHttpChannel %p created nsHttpTransaction %p", function(ch, tr)
+{
+  this.link(ch, tr);
+});
+logan.ruleIf("http request [", state => state.thread.httptransaction, function()
+{
+  this.thread.httptransaction._collecting_request = true;
+});
+logan.plainIf(state => state.thread.httptransaction._collecting_request, function(data)
+{
+  this.prop(this.thread.httptransaction, "request", data.trim() + "\n", true);
+});
+logan.ruleIf("]", state => state.thread.httptransaction._collecting_request, function()
+{
+  this.thread.httptransaction._collecting_request = false;
+  this.thread.httptransaction = null;
+});
+logan.rule("nsHttpConnectionMgr::MakeNewConnection %p ent=%p trans=%p", function(cm, ent, trans)
+{
+  this.prop(trans, "MakeNewConnection", "1");
+});
+logan.rule("  adding transaction to pending queue [trans=%p pending-count=%d]", function(trans, pc)
+{
+  this.state(trans, "pending");
+});
+logan.rule("nsHttpTransaction::HandleContentStart [this=1D7ADC00]", function(trans)
+{
+  this.state(trans, "has-headers");
+  this.thread.httptransaction = trans;
+});
+logan.ruleIf("http response [", state => state.thread.httptransaction, function()
+{
+  this.thread.httptransaction._collecting_response = true;
+});
+logan.plainIf(state => state.thread.httptransaction._collecting_response, function(line)
+{
+  this.prop(this.thread.httptransaction, "response", line.trim() + "\n", true);
+});
+logan.ruleIf("]", state => state.thread.httptransaction._collecting_response, function()
+{
+  this.thread.httptransaction._collecting_response = false;
+  this.thread.httptransaction = null;
+});
+logan.rule("Destroying nsHttpTransaction @%p", function(ptr)
+{
+  this.destroy(ptr);
+});
+logan.summaryProps("nsHttpTransaction", ["state"]);
+
+/******************************************************************************
+ * nsHttpConnection
+ ******************************************************************************/
+
+logan.rule("Creating nsHttpConnection @%p", function(ptr)
+{
+  this.thread.httpconn = this.create(ptr, "nsHttpConnection");
+});
+logan.rule("nsHttpConnection::Activate [this=%p trans=%p caps=%d]", function(conn, trans, caps)
+{
+  this.state(ptr, "activated");
+  this.link(trans, conn);
+});
+logan.rule("nsHttpConnection::OnSocketWritable %p ReadSegments returned [rv=%d read=%d sock-cond=%x again=%d]", function(conn, rv, read, cond, again)
+{
+  if (parseInt(read) > 0)
+    this.state(conn, "sent");
+});
+logan.rule("nsHttpConnection::OnSocketReadable [this=%p]", function(conn)
+{
+  this.state(conn, "recv");
+});
+logan.rule("nsHttpConnection::CloseTransaction[this=%p trans=%p reason=%x]", function(conn, trans, rv)
+{
+  this.state(conn, "done");
+  this.prop("CloseTransaction", trans + ":" + rv + "\n", true);
+});
+logan.rule("Entering Idle Monitoring Mode [this=%p]", function(conn)
+{
+  this.state(conn, "idle");
+});
+logan.rule("Destroying nsHttpConnection @%p", function(ptr)
+{
+  this.destroy(ptr);
+});
+logan.summaryProps("nsHttpConnection", ["state"]);
+
+/******************************************************************************
+ * nsHalfOpenSocket
+ ******************************************************************************/
+
+logan.rule("Creating nsHalfOpenSocket [this=%p trans=%p ent=%s key=%s:%d]",
+  function(ptr, trans, ent, host, port)
+  {
+    this.create(ptr, "nsHalfOpenSocket");
+  });
+logan.rule("nsHalfOpenSocket::OnOutputStreamReady [this=%p ent=%s %s]", function(ptr, end, stream)
+{
+  this.thread.halfopen = ptr;
+});
+logan.ruleIf("nsHalfOpenSocket::SetupConn Created new nshttpconnection %p", state => state.thread.halfopen, function(conn)
+{
+  this.link(this.thread.halfopen, conn);
+  this.thread.halfopen = null;
+});
+logan.rule("Destroying nsHalfOpenSocket [this=%p]", function(ptr)
+{
+  this.destroy(ptr);
+});
--- logan.js
+++ logan.js
@@ -12,16 +12,20 @@ function ensure(array, itemName, def = {})
 }
 
 function isChildFile(file) {
-  return file.name.test(/\.child-\d+(?:\.\d)?$/);
+  return file.name.match(/\.child-\d+(?:\.\d)?$/);
 }
 
 function isRotateFile(file)
 {
-  return file.name.test(/\.\d$/);
+  return file.name.match(/\.\d$/);
+}
+
+function escapeRegexp(s)
+{
+  return s.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
 
 const printfToRegexpMap = {
-  "[\\-\\[\\]\\/\\{\\}\\(\\)\\*\\+\\?\\.\\\\\\^\\$\\|]" : "\\$&",
   "%p": "([A-F0-9]+)",
   "%d": "([\\d]+)",
   "%s": "([^\\s,;]+)",
@@ -30,16 +34,18 @@ const printfToRegexpMap = {
 
 function convertPrintfToRegexp(printf)
 {
+  printf = escapeRegexp(printf);
+
   for (let source in printfToRegexpMap) {
     var target = printfToRegexpMap[source];
-    printf = printf.replace(RegExp(source), target);
+    printf = printf.replace(RegExp(source, "g"), target);
   }
 
   return new RegExp('^' + printf + '$');
 }
 
 const FILE_SLICE = 10 * 1024 * 1024;
-const LINE_MAIN_REGEXP = /^(\d+-\d+-\d+ \d+:\d+:\d+.\d+) \w+ - \[([\w\s#\(\)]+)\]: ([A-Z])\/(\w+) (.*)$/;
+const LINE_MAIN_REGEXP = /^(\d+-\d+-\d+ \d+:\d+:\d+.\d+) \w+ - \[([^\]]+)\]: ([A-Z])\/(\w+) (.*)$/;
 
 // export
 logan = {
@@ -54,17 +60,20 @@ logan = {
         props: {
           className: className,
           pointer: self,
+          state: "init",
         },
         id: logan.objects.length, // unique per all processed log files
         linksTo: [], // lists obj.id
         linkedBy: [], // lists obj.id
+        createFile: logan.filename,
         createOffset: logan.fileoffset,
         createTime: this.timestamp
       };
       logan.objects.push(obj);
       this.objs[self] = obj;
 
-      ensure(logan.searchProps, className, { className: true, pointer: true });
+      // deliberately ommiting the className prop, since that is the first level default only
+      ensure(logan.searchProps, className, { pointer: true, state: true });
 
       return obj;
     },
@@ -77,6 +86,8 @@ logan = {
         return;
       }
 
+      obj.props.state = "released";      
+      obj.destroyFile = logan.filename;
       obj.destroyOffset = logan.fileoffset;
       obj.destroyTime = this.timestamp;
       delete this.objs[obj.props.pointer];
@@ -95,8 +106,8 @@ logan = {
         return;
       }
 
-      src.linksTo.push(trg.id);
-      trg.linkedBy.push(src.id);
+      src.linksTo.push([trg.id, logan.filename, logan.fileoffset]);
+      trg.linkedBy.push([src.id, logan.filename, logan.fileoffset]);
     },
 
     prop: function(self, name, value, merge = false)
@@ -105,8 +116,8 @@ logan = {
       if (!obj) {
         console.warn("Object doesn't exist at " + logan.filename + ":" + logan.linenumber);
         return;
-      }  
-      
+      }
+
       // Update the list of searchable object properties
       ensure(logan.searchProps, obj.props.className)[name] = true;
 
@@ -115,6 +126,11 @@ logan = {
       } else {
         obj.props[name] = value;
       }
+    },
+
+    state: function(self, state)
+    {
+      prop(self, "state", state);
     },
   },
 
@@ -139,28 +155,53 @@ logan = {
   },
 
 
-  // The rest is considered private
-  
-  consumeFiles: function(files)
-  {
-    this.objects = [];
-    this.searchProps = {};
-
-    for (let file of files) {
-      this.consumeFile(file);
-    }
+  _ui: {
+    summary: {}, // map: className -> prop to display on the summary line
   },
 
-  consumeFile: function(file, offset = 0, previousLine = "")
+  summaryProps: function(className, arrayOfProps)
   {
-    if (offset === 0) {
+    this._ui.summary[className] = arrayOfProps;
+  },
+
+  // The rest is considered private
+
+  filesToProcess: [],
+
+  consumeFiles: function(UI, files)
+  {
+    if (this.reader) {
+      this.reader.abort();
+    }
+    this.objects = [];
+    this.searchProps = {};
+    this._proc.global = {};
+
+    this.filesToProcess = Array.from(files);
+
+    this.consumeFile(UI);
+  },
+
+  consumeFile: function(UI, file = null, offset = 0, previousLine = "")
+  {
+    if (!file) {
+      if (!this.filesToProcess.length) {
+        this.reader = null;
+        this.processEOS(UI);
+        return;
+      }
+
+      // a new file to process
+      file = this.filesToProcess.shift();
+      this.eollength = 1;
+      this.linenumber = 0;
+      this.filename = file.name;
+      this.fileoffset = 0;
+
       this._proc.threads = {};
       this._proc.objs = {};
-
-      this.eollength = 1;
-      this.fileoffset = 0;
-      this.linenumber = 0;
-      logan.filename = file.name;
+      this._proc.file = file;
+      this._proc.child = isChildFile(file);
     }
 
     var blob = file.slice(offset, offset + FILE_SLICE);
@@ -168,15 +209,15 @@ logan = {
       if (previousLine) {
         this.consumeLine(file, previousLine);
       }
-      this.processEOF();
+      this.processEOF(UI);
       this._proc.file = null;
+      this.consumeFile(UI);
       return;
     }
 
-    this._proc.file = file;
-
-    var reader = new FileReader();
-    reader.onloadend = function(event) {
+    this.reader = new FileReader();
+    this.reader.onloadend = function(event)
+    {
       if (event.target.readyState == FileReader.DONE) {
         if (offset === 0 && event.target.result.match('\r\n')) {
           this.eollength = 2;
@@ -193,11 +234,17 @@ logan = {
           this.consumeLine(file, line);
         }
 
-        this.consumeFile(file, offset + FILE_SLICE, previousLine);
+        this.consumeFile(UI, file, offset + FILE_SLICE, previousLine);
       }
     }.bind(this);
-    reader.onerror = (event) => { alert(event); }
-    reader.readAsBinaryString(blob);
+
+    this.reader.onerror = function(event)
+    {
+      this.reader = null;
+      alert(event);
+    }.bind(this);
+
+    this.reader.readAsBinaryString(blob);
   },
 
   consumeLine: function(file, line)
@@ -208,7 +255,7 @@ logan = {
 
     var main = line.match(LINE_MAIN_REGEXP);
     if (!main) {
-      this.processLine(this._rules.unmatch, file, text);
+      this.processLine(this._rules.unmatch, file, line);
       return;
     }
 
@@ -224,12 +271,18 @@ logan = {
   processLine: function(ruleSet, file, line)
   {
     for (let rule of ruleSet) { // optmize this!!!
-      if (rule.cond && !rule.cond.bind(this._proc)()) {
+      try {
+        if (rule.cond && !rule.cond(this._proc)) {
+          continue;
+        }
+      } catch (e) {
+        // any exception from the condition checker is ignored
         continue;
       }
 
       if (!rule.regexp) {
-        continue;
+        rule.consumer.call(this._proc, line);
+        return true;
       }
 
       var match = line.match(rule.regexp)
@@ -239,30 +292,259 @@ logan = {
 
       this._proc.match = match;
       rule.consumer.apply(this._proc, match.slice(1));
-
-      // we are here only once per each line of a file when match is hit
-      // hence, it's the place to do any final operations on what has been
-      // done by the matching consumer
       return true;
     }
 
     return false;
   },
 
-  processEOF: function ()
+  processEOF: function(UI)
   {
-    alert("done parsing files");
+  },
+
+  processEOS: function(UI)
+  {
+    UI.fillClassNames(this.searchProps);
+    UI.fillSearchBy();
+  },
+
+  search: function(UI, className, propName, matchValue, match)
+  {
+    var matchFunc;
+    switch (match) {
+      case "exact": {
+        matchFunc = function(prop) { return matchValue === prop; }
+        break;
+      }
+      case "contains": {
+        let contains = new RegExp(escapeRegexp(matchValue), "g");
+        matchFunc = function(prop) { return prop.match(contains); }
+        break;
+      }
+      case "!contains": {
+        let ncontains = new RegExp(escapeRegexp(matchValue), "g");
+        matchFunc = function(prop) { return !prop.match(ncontains); }
+        break;
+      }
+      case "regexp": {
+        let regexp = new RegExp(matchValue, "g");
+        matchFunc = function(prop) { return prop.match(regexp); }
+        break;
+      }
+      case "!regexp": {
+        let nregexp = new RegExp(matchValue, "g");
+        matchFunc = function(prop) { return !prop.match(nregexp); }
+        break;
+      }
+      default:
+        throw "Unexpected match operator";
+    }
+
     for (let obj of this.objects) {
-      console.log(obj.props);
+      if (className != obj.props.className) {
+        continue;
+      }
+      var prop = obj.props[propName] || "";
+      if (!matchFunc(prop)) {
+        continue;
+      }
+      UI.addResult(obj);
     }
   },
 }; // logan impl
 
+var UI =
+{
+  expandedElement: null,
+  results: {},
+
+  setInitialView: function()
+  {
+    $("#file_load_section").removeClass().addClass("section").show();
+    $("#search_section").hide();
+  },
+
+  setSearchView: function(reset)
+  {
+    $("#file_load_section").removeClass().addClass("topbar").show();
+    $("#search_section").show();
+    if (reset) {
+      $("#search_className").empty();
+      $("#search_By").empty();
+      $("#results_section").empty();
+      this.results = {};
+    }
+  },
+
+  setResultsView: function(reset)
+  {
+    $("#search_section").removeClass().addClass("topbar").show();
+    $("#results_section").show();
+    if (reset) {
+      $("#results_section").empty();
+      this.results = {};
+    }
+  },
+
+  fillClassNames: function(classNames)
+  {
+    let select = $("#search_className");
+    for (let className in classNames) {
+      select.append($("<option>").attr("value", className).text(className));
+    }
+  },
+
+  fillSearchBy: function(props)
+  {
+    if (!props) {
+      props = logan.searchProps[$("#search_className").val()] || {};
+    }
+    let select = $("#search_By");
+    select.empty();
+    for (let prop in props) {
+      select.append($("<option>").attr("value", prop).text(prop));
+    }
+  },
+
+  summaryProps: function(obj)
+  {
+    var custom = logan._ui.summary[obj.props.className] || [];
+    return ["className", "pointer"].concat(custom);
+  },
+
+  summary: function(obj)
+  {
+    var props = this.summaryProps(obj);
+    var summary = "";
+    for (let prop of props) {
+      if (summary) summary += " \u2502 ";
+      summary += obj.props[prop] || "-";
+    }
+    return summary;
+  },
+
+  closeExpansion: function(newElement = null)
+  {
+    if (this.expandedElement) {
+      this.expandedElement.remove();
+    }
+    this.expandedElement = newElement;
+  },
+
+  addResult: function(obj, under = null)
+  {
+    if (obj.id in this.results) {
+      // already in the results!  we can alert :)
+      return;
+    }
+
+    this.results[obj.id] = true;
+
+    var summary = this.summary(obj);
+    var element = $("<div>")
+      .addClass("result_summary")
+      .text(summary)
+      .click(function(event)
+      {
+        var expansion = $("<div>").addClass("expansion");
+        this.closeExpansion(expansion);
+
+        var props = this.summaryProps(obj);
+        for (let prop in obj.props) {
+          if (!props.includes(prop)) {
+            var propLines = obj.props[prop].trim().split(/\n/);
+            for (propLine of propLines) {
+              expansion.append($("<div>").addClass("prop").text(
+                prop + ": " + propLine
+              ));
+            }
+          }
+        }
+
+        if (obj.linksTo.length) {
+          expansion.append($("<div>").addClass("label").text("Linking to"));
+        }
+        for (let linkto of obj.linksTo) {
+          var trg = logan.objects[linkto[0]];
+          expansion.append($("<div>").addClass("obj")
+            .text(UI.summary(trg))
+            .click(function(event)
+            {
+              this.closeExpansion();
+              this.addResult(trg, element).click();
+              event.stopPropagation();
+            }.bind(this))
+          );
+        }
+
+        if (obj.linkedBy.length) {
+          expansion.append($("<div>").addClass("label").text("Linked by"));
+        }
+        for (let linkby of obj.linkedBy) {
+          var trg = logan.objects[linkby[0]];
+          expansion.append($("<div>").addClass("obj")
+            .text(UI.summary(trg))
+            .click(function(event)
+            {
+              this.closeExpansion();
+              this.addResult(trg, element).click();
+              event.stopPropagation();
+            }.bind(this))
+          );
+        }
+
+        element.append(expansion);
+        event.stopPropagation();
+      }.bind(this));
+
+    if (!under) {
+      element.addClass("searched");
+      $("#results_section").append(element);
+    } else {
+      element.addClass("added");
+      $("<div>").addClass("indent").append(element).insertAfter(under);
+    }
+    return element;
+  },
+}; // UI
+
 $(() => {
-  $("#files").on("change", (event) => {
-    logan.consumeFiles(event.target.files);
+  $("#files").on("change", (event) =>
+  {
+    UI.setSearchView(true);
+    logan.consumeFiles(UI, event.target.files);
   });
-  logan.consumeFiles($("#files").get()[0].files);
+
+  $("#search_className").on("change", (event) =>
+  {
+    let props = logan.searchProps[event.target.value] || {};
+    UI.fillSearchBy(props);
+  });
+
+  $("#search_button").click((event) =>
+  {
+    UI.setResultsView(true);
+    logan.search(UI,
+      $("#search_className").val(),
+      $("#search_By").val(),
+      $("#search_PropValue").val(),
+      $("#search_Matching").val());
+  });
+
+  $(window).on("keypress", (event) =>
+  {
+    if (event.keyCode == 27) {
+      UI.closeExpansion();
+    }
+  });
+
+  var files = $("#files").get()[0].files;
+  if (files.length) {
+    UI.setSearchView(true);
+    logan.consumeFiles(UI, files);
+  } else {
+    UI.setInitialView();
+  }
 });
 
 })();
